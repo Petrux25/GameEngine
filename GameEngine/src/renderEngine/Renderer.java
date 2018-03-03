903a17964146d8476924226b4ef736923dbf8d67
Diego Velásquez <diesveca@hotmail.com>
2018-03-02 18:18:51 -0600

    Added initial files

---
+++ GameEngine/src/renderEngine/Renderer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renderEngine;
+
+import models.RawModel;
+import models.TexturedModel;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+/**
+ *
+ * @author diesv
+ */
+public class Renderer {
+    public void prepare(){
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GL11.glClearColor(1, 0, 0, 1);
+        
+    }
+    
+    public void render(TexturedModel texturedModel){
+        RawModel model = texturedModel.getRawModel();
+        GL30.glBindVertexArray(model.getVaoID());
+        GL20.glEnableVertexAttribArray(0);
+        GL20.glEnableVertexAttribArray(1);
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D,texturedModel.getTexture().getID() );
+        GL11.glDrawElements(GL11.GL_TRIANGLES,model.getVertexCount(),GL11.GL_UNSIGNED_INT,0);
+        GL20.glDisableVertexAttribArray(0);
+        GL20.glDisableVertexAttribArray(1);
+        GL30.glBindVertexArray(0);
+        
+    }
+}
---
+++ GameEngine/src/textures/ModelTexture.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textures;
+
+/**
+ *
+ * @author diesv
+ */
+public class ModelTexture {
+    private int textureID;
+    
+    public ModelTexture(int id){
+        this.textureID = id;
+    }
+    
+    public int getID(){
+        return this.textureID;
+    }
+    
+    
+}
